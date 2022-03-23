f5579d5de4eea6e6a37877b831f922f2aeb3714f
cevikesra <esracevikk123@gmail.com>
2022-03-23 18:05:01 +0300

    Gezi projesi

---
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        //FeatureManager
+        public IViewComponentResult Invoke()
+        {
+            //var values = featureManager.TGetList();
+      //
+            return View();
+
+        }
+    }
+}
